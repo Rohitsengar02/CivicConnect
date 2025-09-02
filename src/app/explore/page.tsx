e3f32d192d7806977c37dbfb315b14d957105314
Tatajaj <tatajaj912@gmail.com>
2025-09-02 17:03:52 +0000

    create explore page on which show a bar on top for district selection an

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,19 @@
+import { Header } from "@/components/layout/header";
+import { MobileNav } from "@/components/layout/mobile-nav";
+import { ExploreFilters } from "@/components/explore-filters";
+import { IssueGrid } from "@/components/issue-grid";
+
+export default function ExplorePage() {
+  return (
+    <div className="flex min-h-screen w-full flex-col bg-background">
+      <Header />
+      <main className="flex-1 pt-20">
+        <div className="container mx-auto px-4 py-8">
+            <ExploreFilters />
+            <IssueGrid />
+        </div>
+      </main>
+      <MobileNav />
+    </div>
+  );
+}
